60966d28898f848828fb0db8a1a46252ab3d3178
lima <562979315@qq.com>
2023-02-18 01:45:14 +0800

    vue externals : lodash

--- packages/vue3-demo/src/views/HomeView.vue
+++ packages/vue3-demo/src/views/HomeView.vue
@@ -7,12 +7,21 @@
 
 <script lang="ts">
 
-
 import {HelloA,helloConstA} from "@lerna-good/lerna-module-a";
 import {HelloB} from "@lerna-good/lerna-module-b";
 
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+
+import * as _ from 'lodash'
+
+// 转换字符串string为驼峰写法。
+let res = _.camelCase('get_username')
+
+// 在控制台输出运行结果，证明lodash 库被正确引入
+console.log("--------- lodash",res);  // getUsername
+
 
 console.log("--------- vue3 constA",helloConstA)
 
--- packages/vue3-demo/vue.config.js
+++ packages/vue3-demo/vue.config.js
@@ -1,4 +1,71 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+
+  configureWebpack: {
+    externals: {
+        lodash: '_'
+    }
+
+  }
+}
+
+
+//   chainWebpack:config=>{
+//     //发布模式
+//     config.when(process.env.NODE_ENV === 'production',config=>{
+//         //entry找到默认的打包入口，调用clear则是删除默认的打包入口
+//         //add添加新的打包入口
+//         // config.entry('app').clear().add('./src/main-prod.js')
+
+//         config.entry('app').clear().add('./main.ts')
+
+//         //使用externals设置排除项
+//         config.set('externals',{
+//             // vue:'Vue',
+//             // 'vue-router':'VueRouter',
+//             // axios:'axios',
+//             lodash:'_',
+//             // echarts:'echarts',
+//             // nprogress:'NProgress',
+//             // 'vue-quill-editor':'VueQuillEditor'
+//         })
+//     })
+//     //开发模式
+//     config.when(process.env.NODE_ENV === 'development',config=>{
+//         // config.entry('app').clear().add('./src/main-dev.js')
+//         config.entry('app').clear().add('./main.ts')
+//     })
+// }
+// }
+
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+
+//   configureWebpack:config =>{
+//     config.externals = {
+//          // lodash = window._
+//          lodash: "_"
+//     }
+
+//     config.plugins = [
+//       // 指定模板文件
+//       new HtmlWebpackPlugin({
+//         template: "./public/index.html",
+
+//         inject: true,
+//         hash: new Date().getTime(),
+//         url: BASE_URL,  //需要这里传参
+//         minify: {
+//           removeComments: true,
+//           collapseWhitespace: true,
+//           removeAttributeQuotes: true
+//         },
+//         chunksSortMode: 'manual'
+        
+//       }),
+//     ]
+// }
+// })
