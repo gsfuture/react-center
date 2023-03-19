0756a8c56d1a889c94d9c6412ad84182708e5ad5
lima <562979315@qq.com>
2023-03-19 23:51:04 +0800

    iframe in vue3

--- packages/vue-inframe/src/views/AboutView.vue
+++ packages/vue-inframe/src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <iframe src="/preview.html?webId=36" height="800" width="1200" frameborder="0" />
+
+
   </div>
 </template>
